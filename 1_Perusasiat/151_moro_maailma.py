9ac98e15ee7a2d83fb4b708cf9214e85c87ceb93
ozkarii <96316541+ozkarii@users.noreply.github.com>
2023-03-08 20:31:19 +0200

    Delete 1_Perusasiat directory

--- 1_Perusasiat/151_moro_maailma.py
+++
@@ -1,7 +0,0 @@
-"""
-COMP.CS.100 moro maailma
-Tekijä: Oskari Heinonen
-Opiskelijanumero: 151242115
-"""
-
-print("Hello World!")
--- 1_Perusasiat/163_tervehdys.py
+++
@@ -1,9 +0,0 @@
-"""
-COMP.CS.100 tervehdys
-Tekijä: Oskari Heinonen
-Opiskelijanumero: 151242115
-"""
-
-name = input('Tell your name: ')
-
-print("Hi " + str(name) + "\nyour coding skills are great!")
